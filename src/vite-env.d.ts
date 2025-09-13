a16a52a63b0a49f507e183a33a995d6b4aa2933f
Borngreat Ikwutah <158774357+borngreat-ikwutah@users.noreply.github.com>
2025-09-13 15:04:59 +0100

    Added Pinata..

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_MAGIX_TESTNET_CONTRACT_ADDRESS: string;
   readonly VITE_MAGIX_MAINNET_CONTRACT_ADDRESS: string;
   readonly VITE_WC_PROJECT_ID: string;
+  readonly VITE_PINATA_JWT: string;
 }
 
 interface ImportMeta {
